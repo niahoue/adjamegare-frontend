ff92551d35bcd3b10878e96deb69d00bbfb2eac0
Niahoue <apacome343@gmail.com>
2025-09-08 06:38:57 +0000

    correction de la vitesse du site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -60,7 +60,7 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-[#e85805] text-white shadow-md relative z-50">
+    <header className=" bg-[#e85805] text-white shadow-md relative z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-14">
           {/* Logo */}
